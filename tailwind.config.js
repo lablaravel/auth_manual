a5f3ff112e9066037f177426719beada826523ef
Wesllycode <wesllycode07@gmail.com>
2023-05-31 03:51:30 -0300

    :sparkles: feat: precisava chamar @vite pra puxar o css

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,5 +8,18 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  // add daisyUI plugin
+  plugins: [require("daisyui")],
+
+  // daisyUI config (optional)
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
 }
